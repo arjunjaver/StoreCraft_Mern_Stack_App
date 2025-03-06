3d7343e4f9b45af4d6f12cf94e208d9236b927e4
arjunjaver <arjunjaver86@gmail.com>
2025-03-06 14:21:22 +0530

    Updated CORS settings for frontend

--- ecommerce-backend/server.js
+++ ecommerce-backend/server.js
@@ -12,8 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(bodyParser.json());
-app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));;
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL, 
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 
 // MongoDB connection string
 const MONGO_URL = process.env.MONGO_URI;
